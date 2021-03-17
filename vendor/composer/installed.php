c19052d6b5031a1b79681188abecc1eaa3e31331
winston86 <winston86@ukr.net>
2021-03-17 14:17:10 +0200

    Composer config

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '617bab566682d271195206a5e6ff07819faa813d',
+    'name' => 'winston86/completeness',
+  ),
+  'versions' => 
+  array (
+    'winston86/completeness' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '617bab566682d271195206a5e6ff07819faa813d',
+    ),
+  ),
+);
